d0c2d3a9d0ea1fb8334916c1085f128be4e21928
Mehrshad Nosratyar <mehrshadnosratyar@gmail.com>
2023-12-11 14:08:07 +0330

    made sidebar and some features

--- src/Bookcards.jsx
+++ src/Bookcards.jsx
@@ -1,8 +1,16 @@
-function Bookcards({book}) {
+import { useState } from "react";
+
+
+function Bookcards({book,likeList}) {
+    const [hasFav,setFav] = useState(false)
+    function likeHandler(){
+        setFav((Fav) => !Fav)
+        likeList(book,hasFav)
+    }
     return ( 
         <div className="w-full bg-zinc-800 p-3 rounded-lg flex justify-between">
             <a  className="flex flex-row items-center text-white gap-4" href={book.link}>
-            <img className="w-[4.5rem] rounded-sm" src={book.image} alt={book.title} />
+            <img className="w-[4.5rem] rounded-md" src={book.image} alt={book.title} />
             <div className="flex flex-col h-full justify-between">
                 <div className=" flex flex-col gap-5">
                     <h3 className="text-xl">{book.title}</h3>
@@ -18,8 +26,8 @@ function Bookcards({book}) {
                 </div>
             </div>
             </a>
-            <span className="cursor-pointer">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="gainsboro" className="w-7 h-7">
+            <span className="cursor-pointer" onClick={likeHandler}>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill={hasFav ? "red" : "gainsboro"} className="w-7 h-7">
                     <path d="M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z" />
                 </svg>
             </span>
--- src/Presenter.jsx
+++ src/Presenter.jsx
@@ -1,14 +1,30 @@
+import { useState } from "react";
 import Bookcards from "./Bookcards";
+import Favbooks from "./Favbooks";
+
 
 function Presenter({books}) {
+    const [likedList,setLikeList] = useState([])
+
+    function likeList(book,status){
+        if(status){
+            const newLikeList = likedList.filter(i => i.id!== book.id)
+            setLikeList(newLikeList)
+        } else{
+            setLikeList(likedList => [...likedList,book])
+        }
+    }
     return ( 
-        <div className="flex">
-            <div className="flex flex-col gap-6 w-full overflow-hidden">
+        <div className="flex gap-10">
+            <div className={"flex flex-col gap-6 w-full overflow-hidden"}>
                 {books.map((book) => (
-                    <Bookcards key={books.id} book={book} />
+                    <Bookcards key={book.id} book={book} likeList={likeList} />
                 ))}
             </div>
-            <div></div>
+            {!!likedList.length && <div className="w-96 text-white bg-violet-700 py-2 h-max px-4 rounded-xl flex flex-col gap-4">
+                <h2 className="text-3xl text-fuchsia-200">Favorite List</h2>
+                {likedList.map((book) => ( <Favbooks key={book.id} book={book} /> ))}
+            </div>}
         </div>
      );
 }
