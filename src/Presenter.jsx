d083eb58f2e309563d8a521bb4b5c32f9e56f0ed
Mehrshad Nosratyar <mehrshadnosratyar@gmail.com>
2023-12-11 14:19:02 +0330

    some responsive design

--- src/Presenter.jsx
+++ src/Presenter.jsx
@@ -15,16 +15,17 @@ function Presenter({books}) {
         }
     }
     return ( 
-        <div className="flex gap-10">
+        <div className="flex gap-10 flex-col md:flex-row-reverse">
+            {!!likedList.length && <div className="w-full md:w-96 text-white bg-violet-700 py-2 h-max px-4 rounded-xl flex flex-col gap-4">
+                <h2 className="text-3xl text-fuchsia-200">Favorite List</h2>
+                {likedList.map((book) => ( <Favbooks key={book.id} book={book} /> ))}
+            </div>}
             <div className={"flex flex-col gap-6 w-full overflow-hidden"}>
                 {books.map((book) => (
                     <Bookcards key={book.id} book={book} likeList={likeList} />
                 ))}
             </div>
-            {!!likedList.length && <div className="w-96 text-white bg-violet-700 py-2 h-max px-4 rounded-xl flex flex-col gap-4">
-                <h2 className="text-3xl text-fuchsia-200">Favorite List</h2>
-                {likedList.map((book) => ( <Favbooks key={book.id} book={book} /> ))}
-            </div>}
+        
         </div>
      );
 }
