83c3e43959c193f01b1a7c4b300a3e207e2ad6cc
Mehrshad Nosratyar <mehrshadnosratyar@gmail.com>
2023-12-11 16:08:15 +0330

    completed search box and project

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
 import Header from "./Header";
 import Presenter from "./Presenter";
 import Searchbar from "./Searchbar";
-import {books}  from "./constants/mockData";
-
+import {books as bookData}  from "./constants/mockData";
+import { useState } from "react";
 export default function App() {
+  const [search,setSearch] = useState("")
+  const [books,setBooks] = useState(bookData)
+
+  function searchHandler() {
+    if(search){
+    const selectedBooks = books.filter((book)=> (book.title.toLowerCase().includes(search)))
+    setBooks(selectedBooks)
+    } else setBooks(bookData)
+  }
+
+
+
     return (
       <div className="container w-3/4 p-3 mx-auto ">
         <Header/>
-        <Searchbar/>
+        <Searchbar search={search} setSearch={setSearch} searchHandler={searchHandler}/>
         <Presenter books={books}/>
       </div>
     )
--- src/Searchbar.jsx
+++ src/Searchbar.jsx
@@ -2,13 +2,16 @@
 
 
 
+function Searchbar({search,setSearch,searchHandler}) {
 
-function Searchbar() {
+    const changeHandler = (e) =>{
+        setSearch(e.target.value.toLowerCase())
+    }
     return(
     <div className="flex gap-2 items-center my-14 p-5">
-        <input type="text" name="serachbar" placeholder="Search title" className=" pl-4 h-10 w-64 rounded-md bg-purple-50 border-purple-900" />
-        <span className="text-white bg-purple-400 w-10 h-10 flex items-center justify-center rounded-md transition-all">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+        <input type="text" value={search} onChange={changeHandler} name="serachbar" placeholder="Search title" className=" pl-4 h-10 w-64 rounded-md bg-purple-50 border-purple-900" />
+        <span onClick={searchHandler} className="text-white cursor-pointer bg-purple-400 w-10 h-10 flex items-center justify-center rounded-md transition-all">
+            <svg  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
             <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
             </svg>
         </span>
