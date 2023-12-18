289c83879cd0da6a9a536900c448ac7e9506eb0a
Mehrshad Nosratyar <mehrshadnosratyar@gmail.com>
2023-12-18 12:18:28 +0330

    some changes

--- src/Favbooks.jsx
+++ src/Favbooks.jsx
@@ -4,7 +4,7 @@ function Favbooks({book}) {
     if(book){
         return (
             <div>
-                <div className="flex items-center  gap-3 bg-violet-500 p-2 rounded-lg">
+                <div className="flex items-center  gap-3 bg-purple-500 p-2 rounded-lg">
                     <img className="w-16 rounded-md" src={book.image} alt={book.title} />
                     <h4>{book.title}</h4>
                 </div>
--- src/Presenter.jsx
+++ src/Presenter.jsx
@@ -16,8 +16,8 @@ function Presenter({books}) {
     }
     return ( 
         <div className="flex gap-10 flex-col md:flex-row-reverse">
-            {!!likedList.length && <div className="w-full md:w-96 text-white bg-violet-700 py-2 h-max px-4 rounded-xl flex flex-col gap-4">
-                <h2 className="text-3xl text-fuchsia-200">Favorite List</h2>
+            {!!likedList.length && <div className="w-full md:w-96 text-white bg-purple-400 py-2 h-max px-4 rounded-xl flex flex-col gap-4">
+                <h2 className="text-3xl text-fuchsia-100">Favorite List</h2>
                 {likedList.map((book) => ( <Favbooks key={book.id} book={book} /> ))}
             </div>}
             <div className={"flex flex-col gap-6 w-full overflow-hidden"}>
